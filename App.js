ffd80da89df88056b846820713ac3241b3f440fb
Benigne27 <benigneblessed@gmail.com>
2024-02-12 22:19:54 +0200

    First steps in React Native

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { styles } from './style';
 
 export default function App() {
+    let isLoading=false
+
+    const whenUserClick=()=>{
+      isLoading=true
+      console.log(isLoading)
+    }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Text style={styles.text}>Hello World!</Text>
+
       <StatusBar style="auto" />
+      
+      <TouchableOpacity style={styles.Button} onPress={whenUserClick()}>
+        <Text style={{color: 'green'}}>Click here</Text>
+      </TouchableOpacity>
+
+      {isLoading?(
+        <ActivityIndicator  />
+      ):(
+        <Text>Open up App.js to start working on your app!</Text>
+      )}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ style.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "gray",
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  
+    text:{
+      color:'#fff',
+      fontSize:20,
+      fontWeight:"bold",
+      fontStyle:"italic"
+    },
+    Button:{
+      padding: 10,
+      backgroundColor: "yellow",
+      
+    }
+  
+  });
